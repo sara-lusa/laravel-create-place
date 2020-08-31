478ad0c647f847264683de5878d064659612476e
Sara <saracorsob@gmail.com>
2020-08-31 15:52:31 +0200

    Scrittura controller

--- app/Http/Controllers/Student.php
+++ app/Http/Controllers/Student.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 class Student extends Controller
 {
   public function index() {
-    return view('students');
+    $students = [
+      [
+        'nome' => 'Sara',
+        'cognome' => 'Lusa',
+        'Media' => '8'
+      ],
+      [
+        'nome' => 'Anna',
+        'cognome' => 'Rossi',
+        'Media' => '8'
+      ],
+      [
+        'nome' => 'Viola',
+        'cognome' => 'Berti',
+        'Media' => '8'
+      ],
+    ];
+    // dd(['data' => $students]);
+    // dd(compact('students'));
+
+    return view('students', compact('students'));
   }
 }
